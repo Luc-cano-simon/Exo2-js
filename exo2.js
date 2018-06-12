e29a5ed249e2250540e1463d79f64f2d7f96688d
luc <luc.cano@hotmail.fr>
2018-06-12 13:46:24 +0200

    exercice8

--- exo2.js
+++ exo2.js
@@ -1,10 +1,24 @@
+												/* Exo 1 */
+
 var mois = ['Janvier','Fevrier','Mars','Avril', 'Mai','Juin','Juillet','Aout', 'Septembre', 'Octobre','Novembre','Decembre'];
+/*alert(mois); */
+
+												/* Exo 2,3,4 */
 
 document.write(mois[2]+ "<br/>" + "<br/>"); /* affiche la valeur " MARS " (la première valeur = à O) */
 document.write(mois[4]+ "<br/>" + "<br/>");/* affiche la valeur " MAI " (la première valeur = à O) */
 document.write(mois[10]+ "<br/>" + "<br/>");/* affiche la valeur " Novembre " (la première valeur = à O) */
 
+
 /*
 mois[7] = "Aôut" /* création d'une variable 
 alert("le moi d " +(mois[7])+ " vient d'être modifier"); /* alert de modification 
 */
+
+/* mois[7]= "aôut";
+document.write(mois); 
+*/
+
+var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
+array.push('courgette')
+document.write(array) ;
