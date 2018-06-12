f7df13361f64b80058deebdfee6664e5e8e35b3e
luc <luc.cano@hotmail.fr>
2018-06-12 12:00:36 +0200

    exercice2

--- exo2.js
+++ exo2.js
@@ -1,2 +1,6 @@
 var mois = ['Janvier','Fevrier','Mars','Avril', 'Mai','Juin','Juillet','Aout', 'Septembre', 'Octobre','Novembre','Decembre'];
-alert (mois);
+
+document.write(mois[2]+ "<br/>" + "<br/>"); /* affiche la valeur " MARS " (la première valeur = à O) */
+document.write(mois[4]+ "<br/>" + "<br/>");/* affiche la valeur " MAI " (la première valeur = à O) */
+document.write(mois[10]+ "<br/>" + "<br/>");/* affiche la valeur " Novembre " (la première valeur = à O) */
+
