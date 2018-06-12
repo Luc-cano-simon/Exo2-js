aa730e765d2bbd080d4382318ff8b54bac9def1b
luc <luc.cano@hotmail.fr>
2018-06-12 12:28:51 +0200

    exercice 5

--- exo2.js
+++ exo2.js
@@ -4,3 +4,7 @@ document.write(mois[2]+ "<br/>" + "<br/>"); /* affiche la valeur " MARS " (la pr
 document.write(mois[4]+ "<br/>" + "<br/>");/* affiche la valeur " MAI " (la première valeur = à O) */
 document.write(mois[10]+ "<br/>" + "<br/>");/* affiche la valeur " Novembre " (la première valeur = à O) */
 
+/*
+mois[7] = "Aôut" /* création d'une variable 
+alert("le moi d " +(mois[7])+ " vient d'être modifier"); /* alert de modification 
+*/
